1c3a12fcd048c723a96f1e19b4f07f02ed726d96
GurvinderGill25 <ginnyseehra@hotmail.com>
2025-12-10 00:48:40 -0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,50 @@
+#Algorithm
+#Display the your name as the name of the author
+#Display your student ID
+#Display the name of the program
+#Request and store the first number to be used as variable X
+#Request and store the second number to be used as variable Y
+#Calculate Addition = X + Y
+#Display the result of Addition
+#Calculate Subtraction= X-Y
+#Display result of Subtraction
+#Calculate Multiplication = X*Y
+#Display result of Multiplication
+#Calculate Division= X/Y
+#Display result of Division 
+#Calculate the Exponent i.e. X^Y = X**Y
+#Display the result of Exponent
+#Calculate the remainder of X/Y = X%Y
+#Display the Remainder 
+#Stop the program
+
+print("Program Author: Gurvinder Gill ") #Displaying author's name
+print("Student ID: 3719201 ")  #Displaying author's student number
+print("Program 1 - Math Functions") #Displaying the name of the program
+
+
+X = float(input("The first number is: ")) #Getting the first number from the user and storing it as a float which allows the user to enter  both integers and decimals.
+Y = float(input("The second number is: ")) #Getting the second number from the user and storing it as a float too which allows the user to enter  both integers and decimals.
+
+Addition = X+Y #Calculationg the sum of both the numbers and storing it as Addition
+print("Addition = ",Addition)  #Displaying the sum of both the numbers
+
+Subtraction = X-Y #Calculating the difference of both the numbers and storing it as Subtraction
+print("Subtraction = ",Subtraction) #Displaying the difference of both the numbers
+
+Multiplication = X*Y #Calculating the product of both the numbers and storing it as Multiplication
+print("Multiplication = ",Multiplication) #Displaying the product of both the numbers
+
+Division = X/Y #Calculating the quotient by dividing the first number by the second and storing it as Division
+print("Division = ",Division) #Displaying the quotient obtained by dividing the first number by the second
+
+Exponent = X**Y #Calculating the value of the first number raised to the power of the second i.e. X^Y and storing it as Exponent
+print("Exponent = ",Exponent) #Displaying the value of the first number raised to the power of the second i.e. X^Y
+
+Remainder = X%Y #Calculating the remainder obtained by dividing the first number by the second and storing it as Remainder
+print("Remainder = ",Remainder) #Displaying the remainder obtained by dividing the first number by the second
+
+
+
+
+
